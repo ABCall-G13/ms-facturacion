032458b117f02aa7cf0740becd5ba1edf97221c2
alvarez-alberto <alberto.alvarez512@gmail.com>
2024-11-09 17:28:55 -0500

    base app

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_USER = os.getenv("DB_USER", "prueba")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "prueba")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "prueba")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_SOCKET_PATH_PRIMARY = os.getenv("DB_SOCKET_PATH_PRIMARY", "")
+URL_SERVICE_CLIENT = os.getenv("URL_SERVICE_CLIENT", "http://localhost:8000")
